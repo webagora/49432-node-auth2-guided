c90c964675dea170c35e1d8914d5f8780623e0b7
Ladrillo <gabriel@cabrejas.net>
2022-01-25 19:48:11 +0100

    committing to "lecture"

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -7,12 +7,16 @@ const {
   checkRole,
 } = require('../auth/auth-middleware')
 
-router.get("/", restricted, checkRole('admin'), (req, res, next) => {
-  User.find()
-    .then(users => {
-      res.json(users)
-    })
-    .catch(next) // our custom err handling middleware in server.js will trap this
-})
+router.get(
+  "/",
+  restricted,
+  checkRole('admin'),
+  (req, res, next) => {
+    User.find()
+      .then(users => {
+        res.json(users)
+      })
+      .catch(next) // our custom err handling middleware in server.js will trap this
+  })
 
 module.exports = router
