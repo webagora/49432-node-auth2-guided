1727b84f5fe06670301a7aaac2e9166d5645234d
Ladrillo <gabriel@cabrejas.net>
2022-01-25 18:22:41 +0100

    committing to "lecture"

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -27,6 +27,7 @@ router.post('/login', (req, res, next) => {
     .then(([user]) => {
       if (user && bcrypt.compareSync(password, user.password)) {
         // here we make token and send it to client in res.body
+        // const token = makeToken(user)
         res.status(200).json({ message: `Welcome back ${user.username}...` })
       } else {
         next({ status: 401, message: 'Invalid Credentials' })
