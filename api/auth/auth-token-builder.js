cb99b452f88790cf9ad75358210eaf2e413cadf0
Ladrillo <gabriel@cabrejas.net>
2022-01-25 18:32:13 +0100

    committing to "lecture"

--- api/auth/auth-token-builder.js
+++ api/auth/auth-token-builder.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken')
-const JWT_SECRET = require('../')
+const JWT_SECRET = require('../../config').JWT_SECRET
 
 function tokenBuilder(user) {
   return 'the awesome token'
