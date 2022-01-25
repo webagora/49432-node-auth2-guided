7bda1ad0f0bf7ea1073202bd69150d405c0d4d24
Ladrillo <gabriel@cabrejas.net>
2022-01-25 19:15:16 +0100

    committing to "lecture"

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -6,10 +6,9 @@ const restricted = (req, res, next) => {
   // pull token from request ???
   const token = req.headers.authorization
   if (!token) {
-    next({ status: 401, message: 'You shall not pass!' })
-  } else {
-    
+    return next({ status: 401, message: 'You shall not pass!' })
   }
+  jwt.verify()
 }
 
 // AUTHORIZATION
