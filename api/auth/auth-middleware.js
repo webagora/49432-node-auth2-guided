c25f07f88a720349d381116cd525621f4a5fc031
RFNoC <rfnoc@rfnocs-pro-51.lan>
2022-01-25 17:07:28 -0800

    api/auth/auth-middleware.js: restricted build

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,6 +1,21 @@
+const jwt = require('jsonwebtoken')
+const { JWT_SECRET } = require('../../config')
+
 // AUTHENTICATION
 const restricted = (req, res, next) => {
-  next()
+  const token = req.headers.authorization
+  if (!token) {
+    next({ status: 401, message: 'You shall not pass!' })
+  } else {
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+      if (err) {
+        next({ status: 401, message: `Your token sucks: ${err.message}` })
+      } else {
+        req.decodedJwt = decoded
+        next()
+      }
+    })
+  }
 }
 
 // AUTHORIZATION
