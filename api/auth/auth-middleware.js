ea3344ac258e338d5ee4873b0a283c8107cd391a
RFNoC <rfnoc@rfnocs-pro-51.lan>
2022-01-25 17:56:44 -0800

    api/auth/auth-middleware.js: checkRole - build

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -19,8 +19,12 @@ const restricted = (req, res, next) => {
 }
 
 // AUTHORIZATION
-const checkRole = (req, res, next) => {
-  next()
+const checkRole = role => (req, res, next) => {
+  if (req.decodedJwt.role === role) {
+    next()
+  } else {
+    next({ status: 403, message: 'you have no power here!' })
+  }
 }
 
 module.exports = {
